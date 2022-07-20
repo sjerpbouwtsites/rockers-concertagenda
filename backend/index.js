206e0dd5c5b0486fb09f66bf2947aab93ed3b45f
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2022-07-20 19:20:11 +0200

    20-7

--- backend/index.js
+++ backend/index.js
@@ -121,7 +121,7 @@ function init() {
     workerList.push([fsDirections.scrapeGebrdenobel, "gebrdenobel", 0]);
   }
 
-  if (EventsList.isOld("neushoorn")) {
+  if (EventsList.isOld("neushoorn") || true) {
     workerList.push([fsDirections.scrapeNeushoorn, "neushoorn", 0]);
   }
 
