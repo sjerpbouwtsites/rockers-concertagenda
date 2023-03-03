1e451a30cb6a2058c94c8ba77d953c1c24111d99
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2023-03-03 22:58:35 +0100

    maxSimultaneousWorkers setting on workerStatus

--- backend/mods/WorkerStatus.js
+++ backend/mods/WorkerStatus.js
@@ -20,6 +20,7 @@ export default class WorkerStatus {
   static _workers = {};
   static CPUFree = 100;
   static shellArguments = getShellArguments();
+  static maxSimultaneousWorkers = 6;
 
   /**
    * Niet een dynamische waarde maar éénmalig ingesteld
