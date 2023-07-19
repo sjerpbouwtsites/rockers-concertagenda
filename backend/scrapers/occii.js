56e8043f2677781810ff12ce87fae3212b66b446
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2023-07-19 06:50:24 +0200

    occii hmtl

--- backend/scrapers/occii.js
+++ backend/scrapers/occii.js
@@ -207,9 +207,177 @@ occiiScraper.getPageInfo = async function ({ page, event}) {
 
     res.genre = Array.from(document.querySelectorAll('.event-categories [href*="events/categories"]')).map(cats => cats.textContent.toLowerCase().trim())
 
-    document.querySelector(".occii-event-notes h1")?.parentNode.removeChild(document.querySelector(".occii-event-notes h1"))
-    
-    res.longTextHTML = document.querySelector(".occii-event-notes").innerHTML;
+    // #region [rgba(100, 0, 0, 0.3)] longHTML
+      
+    const textSelector = '.occii-event-notes';
+    const mediaSelector = [
+      `${textSelector} [itemprop='video']`,
+      `${textSelector} iframe[src*='bandcamp']`,
+    ].join(", ");
+    const removeEmptyHTMLFrom = textSelector;
+    const socialSelector = [
+      `${textSelector} [href*='instagram']`,
+      `${textSelector} [href*='facebook']`,
+      `${textSelector} [href*='fb.me']`,
+      `${textSelector} a[href*='bandcamp.com']`,
+    ].join(", ");
+    const removeSelectors = [
+      "[class*='icon-']",
+      "[class*='fa-']",
+      ".fa",
+      `${textSelector} [href*='instagram']`,
+      `${textSelector} [href*='facebook']`,
+      `${textSelector} [href*='fb.me']`,      
+      `${textSelector} a[href*='bandcamp.com']`,
+      `${textSelector} script`,
+      `${textSelector} noscript`,
+      `${textSelector} style`,
+      `${textSelector} meta`,
+      `${textSelector} h1`,
+      `${textSelector} img`,
+      `${textSelector} [itemprop="video"]`,
+    ].join(", ");
+
+    const attributesToRemove = [
+      "style",
+      "hidden",
+      "_target",
+      "frameborder",
+      "onclick",
+      "aria-hidden",
+      "allow",
+      "allowfullscreen",
+      "data-deferlazy",
+      "width",
+      "height",
+    ];
+    const attributesToRemoveSecondRound = ["class", "id"];
+    const removeHTMLWithStrings = [];
+
+    // eerst onzin attributes wegslopen
+    const socAttrRemSelAdd = `${
+      socialSelector.length ? `, ${socialSelector}` : ""
+    }`;
+    const mediaAttrRemSelAdd = `${
+      mediaSelector.length ? `, ${mediaSelector} *, ${mediaSelector}` : ""
+    }`;      
+    document
+      .querySelectorAll(`${textSelector} *${socAttrRemSelAdd}${mediaAttrRemSelAdd}`)
+      .forEach((elToStrip) => {
+        attributesToRemove.forEach((attr) => {
+          if (elToStrip.hasAttribute(attr)) {
+            elToStrip.removeAttribute(attr);
+          }
+        });
+      });
+
+   
+    //media obj maken voordat HTML verdwijnt
+    res.mediaForHTML = !mediaSelector.length ? '' : Array.from(
+      document.querySelectorAll(mediaSelector)
+    ).map((bron) => {
+      bron.className = "";
+
+      if (bron?.src && bron.src.includes('bandcamp')){
+        return {
+          outer: bron.outerHTML,
+          src: bron.src,
+          id: null,
+          type: 'bandcamp'
+        }
+      }
+        
+      return {
+        outer: null,
+        src: null,
+        id: bron.id.match(/_(.*)/)[1],
+        type: "youtube",
+      };
+ 
+      // terugval???? nog niet bekend met alle opties.
+    //   return {
+    //     outer: bron.outerHTML,
+    //     src: bron.src,
+    //     id: null,
+    //     type: bron.src.includes("spotify")
+    //       ? "spotify"
+    //       : bron.src.includes("youtube")
+    //         ? "youtube"
+    //         : "bandcamp",
+    //   };
+    });
+
+    //socials obj maken voordat HTML verdwijnt
+    res.socialsForHTML = !socialSelector
+      ? ""
+      : Array.from(document.querySelectorAll(socialSelector)).map((el) => {
+        el.querySelectorAll("i, svg, img").forEach((rm) =>
+          rm.parentNode.removeChild(rm)
+        );
+
+        if (!el.textContent.trim().length) {
+          if (el.href.includes("facebook")) {
+            el.textContent = "Facebook";
+          } else if (el.href.includes("twitter")) {
+            el.textContent = "Tweet";
+          } else {
+            el.textContent = "Onbekende social";
+          }
+        }
+        el.className = "";
+        el.target = "_blank";
+        return el.outerHTML;
+      });
+
+    // stript HTML tbv text
+    removeSelectors.length &&
+        document
+          .querySelectorAll(removeSelectors)
+          .forEach((toRemove) => toRemove.parentNode.removeChild(toRemove));
+
+    // verwijder ongewenste paragrafen over bv restaurants
+    Array.from(
+      document.querySelectorAll(
+        `${textSelector} p, ${textSelector} span, ${textSelector} a`
+      )
+    ).forEach((verwijder) => {
+      const heeftEvilString = !!removeHTMLWithStrings.find((evilString) =>
+        verwijder.textContent.includes(evilString)
+      );
+      if (heeftEvilString) {
+        verwijder.parentNode.removeChild(verwijder);
+      }
+    });
+
+    // lege HTML eruit cq HTML zonder tekst of getallen
+    document
+      .querySelectorAll(`${removeEmptyHTMLFrom} > *`)
+      .forEach((checkForEmpty) => {
+        const leegMatch = checkForEmpty.innerHTML
+          .replace("&nbsp;", "")
+          .match(/[\w\d]/g);
+        if (!Array.isArray(leegMatch)) {
+          checkForEmpty.parentNode.removeChild(checkForEmpty);
+        }
+      });
+
+    // laatste attributen eruit.
+    document.querySelectorAll(`${textSelector} *`).forEach((elToStrip) => {
+      attributesToRemoveSecondRound.forEach((attr) => {
+        if (elToStrip.hasAttribute(attr)) {
+          elToStrip.removeAttribute(attr);
+        }
+      });
+    });
+
+    // tekst.
+    res.textForHTML = Array.from(document.querySelectorAll(textSelector))
+      .map((el) => el.innerHTML)
+      .join("");
+
+    // #endregion longHTML
+
+
     return res;
   }, {months: getVenueMonths('occii'), event}); //TODO is verouderde functie getVenueMonths
 
