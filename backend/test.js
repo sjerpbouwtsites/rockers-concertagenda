1a3b1a2de13ae4eb4784a9527a59d34fa18ee646
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2023-07-26 01:27:49 +0200

    nieuwe nor

--- backend/test.js
+++ backend/test.js
@@ -1,27 +1,27 @@
-const metalEncUrl = `https://www.metal-archives.com/search/ajax-band-search/?field=name&query=ghost`;
-const foundInMetalEncyclopedia = await fetch(metalEncUrl)
-  .then((result) => result.json())
-  .then((parsedJson) => {
-    if (parsedJson.iTotalRecords < 1) return false
-    const bandNamesAreMainTitle = parsedJson.aaData.some(bandData => {
-      let match;
-      try {
-        match = bandData[0].match(/>(.*)<\//);
-        if (Array.isArray(match) && match.length > 1){
-          return match[1].toLowerCase() === 'ghost';
-        } 
-      } catch (error) {
-        return false
-      }
-      return false;
-    });
-    return bandNamesAreMainTitle
-  });
-console.log(foundInMetalEncyclopedia)
-// if (foundInMetalEncyclopedia) {
-//   return {
-//     event,
-//     success: true,
-//     reason: `found in <a class='single-event-check-reason metal-encyclopedie metal-encyclopedie--success' href='${metalEncUrl}'>metal encyclopedia</a>`
-//   };
-// }
+import fs from 'fs';
+
+
+const locatiesMetalfan = JSON.parse(fs.readFileSync('./event-lists/metalfan/0.json')).map(event => {
+  return {
+    location: event.location,
+    title: event.title
+  }
+})
+
+const aantalPerLocatie = {};
+
+locatiesMetalfan.forEach(loc=>{
+  const ll = loc.location;
+  if (!aantalPerLocatie[ll]){
+    aantalPerLocatie[ll] = {
+      count: 1,
+      names: []
+    }
+    aantalPerLocatie[ll].names.push(loc.title)
+    return 
+  }
+  aantalPerLocatie[ll].count = aantalPerLocatie[ll].count + 1;
+  aantalPerLocatie[ll].names.push(loc.title)
+})
+
+console.log(aantalPerLocatie)
