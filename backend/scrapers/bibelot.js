92d22ef93a423364ca9eace2c4e0d26393d10d6b
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-10-06 05:12:20 +0200

    eslint refactor

--- backend/scrapers/bibelot.js
+++ backend/scrapers/bibelot.js
@@ -66,7 +66,7 @@ bibelotScraper.mainPage = async function () {
     const thisWorkersEvents = availableBaseEvents.filter(
       (eventEl, index) => index % workerData.workerCount === workerData.index,
     );
-    return await this.mainPageEnd({ stopFunctie: null, rawEvents: thisWorkersEvents });
+    return this.mainPageEnd({ stopFunctie: null, rawEvents: thisWorkersEvents });
   }
 
   const { stopFunctie, page } = await this.mainPageStart();
@@ -105,7 +105,7 @@ bibelotScraper.mainPage = async function () {
   const thisWorkersEvents = rawEvents.filter(
     (eventEl, index) => index % workerData.workerCount === workerData.index,
   );
-  return await this.mainPageEnd({ stopFunctie, page, rawEvents: thisWorkersEvents });
+  return this.mainPageEnd({ stopFunctie, page, rawEvents: thisWorkersEvents });
 };
 // #endregion                          MAIN PAGE
 
@@ -213,7 +213,7 @@ bibelotScraper.singlePage = async function ({ page, event }) {
   pageInfo.socialsForHTML = socialsForHTML;
   pageInfo.textForHTML = textForHTML;
 
-  return await this.singlePageEnd({
+  return this.singlePageEnd({
     pageInfo,
     stopFunctie,
     page,
