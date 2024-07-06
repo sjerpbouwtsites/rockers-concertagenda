a171cf0bcb92c41fe3dffab0dffd5086c136605c
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2024-07-06 07:53:23 +0200

    onderhoud artist db sorteer op lengte

--- backend/artist-db/models/onderhoud.js
+++ backend/artist-db/models/onderhoud.js
@@ -3,14 +3,15 @@ import fs from 'fs';
 function sorteerStoreOpLengteVanKeys() {
   const storeFiles = ['allowed-artists.json', 'allowed-events.json', 'refused.json', 'unclear-artists.json'];
   storeFiles.forEach((store) => {
-    const file = JSON.parse(fs.readdirSync(`../store/${store}`));
+    const file = JSON.parse(fs.readFileSync(`../store/${store}`, 'utf-8'));
     const fileKeys = Object.keys(file);
     const sortedKeys = fileKeys.sort((a, b) => {
       if (a.length > b.length) return -1;
       if (b.length > a.length) return 1;
       return 0;
     });
-    const nieuweFile = sortedKeys.forEach((key) => {
+    const nieuweFile = {};
+    sortedKeys.forEach((key) => {
       nieuweFile[key] = file[key];
     });
     fs.writeFileSync(`../store/${store}`, JSON.stringify(nieuweFile, null, 2), 'utf-8');
