ff3af0e397573b4c43958e8d3c0f9461c82efb0f
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2024-12-24 06:50:42 +0100

    paradiso fix

--- draai.sh
+++ draai.sh
@@ -1,6 +1,7 @@
-cd /home/sjerp/dev/apache/concertagenda/backend;\
-killall -s 9 chrome; clear;\
+cd /home/sjerp/hosted/rockagenda/backend
+killall -s 9 chrome
+clear
 node index.js workers=3 \
-keepImages=true \
-DONTkeepBaseEvents=true \
-force=all;
+    keepImages=true \
+    DONTkeepBaseEvents=true \
+    force=all
