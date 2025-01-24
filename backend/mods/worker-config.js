34803590f0fb1593c9c2c2d69427e58db89ce4a3
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2025-01-24 04:52:14 +0100

    metalfan

--- backend/mods/worker-config.js
+++ backend/mods/worker-config.js
@@ -60,29 +60,29 @@ export const workerConfig = {
   // },
   // groeneengel: { workerCount: 1, workerConcurrent: 1, forceSet: 3 },
   // iduna: { workerCount: 1, workerConcurrent: 1, forceSet: 4 },
-  kavka: {
-    workerCount: 1,
-    workerConcurrent: 2,
-    forceSet: 4,
-  },
+  // kavka: {
+  //   workerCount: 1,
+  //   workerConcurrent: 2,
+  //   forceSet: 4,
+  // },
   // melkweg: {
   //   workerCount: 2,
   //   CPUReq: 'high',
   //   workerConcurrent: 1,
   //   forceSet: 4,
   // },
-  // littledevil: {
-  //   workerCount: 1,
-  //   workerConcurrent: 2,
-  //   forceSet: 4,
-  // },
-  // metalfan: { workerCount: 1, CPUReq: 'low', workerConcurrent: 1 },
-  // metropool: {
-  //   workerCount: 2,
-  //   workerConcurrent: 1,
-  //   CPUReq: 'high',
-  //   forceSet: 4,
-  // },
+  littledevil: {
+    workerCount: 1,
+    workerConcurrent: 2,
+    forceSet: 4,
+  },
+  metalfan: { workerCount: 1, CPUReq: 'low', workerConcurrent: 1 },
+  metropool: {
+    workerCount: 2,
+    workerConcurrent: 1,
+    CPUReq: 'high',
+    forceSet: 4,
+  },
   // neushoorn: { workerCount: 2, workerConcurrent: 1, forceSet: 5 },
   // nieuwenor: { workerCount: 1, workerConcurrent: 2, forceSet: 5 },
   // occii: {
