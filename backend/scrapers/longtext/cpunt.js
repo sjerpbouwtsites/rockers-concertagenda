1117cd9a79cb790abc7c53eda2bf57cf711d0a54
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2025-04-15 16:45:44 +0200

    dbs longtext nieuw

--- backend/scrapers/longtext/cpunt.js
+++ backend/scrapers/longtext/cpunt.js
@@ -28,18 +28,6 @@ export default async function longTextSocialsIframes(page, event) {
         mediaEls: ".contentblock-Video iframe",
         removeEmptyHTMLFrom: ".contentblock-TextOneColumn"
     };
-    selectors.removeEls.concat([
-        ".post-content h3",
-        ".post-content .wpt_listing",
-        ".post-content .su-youtube",
-        ".post-content .su-spotify",
-        ".post-content .su-button",
-        ".post-content h2 a[href*='facebook']",
-        ".post-content h2 a[href*='instagram']",
-        ".post-content h2 a[href*='bandcamp']",
-        ".post-content h2 a[href*='spotify']",
-        ".post-content .su-button-center"
-    ]);
 
     res.mediaForHTML = await maakMediaHTMLBronnen(page, selectors, event);
 
--- backend/scrapers/longtext/dbs.js
+++ backend/scrapers/longtext/dbs.js
@@ -1,132 +1,49 @@
 /* eslint-disable  */
 /* eslint-disable indent */
 /* global document */
+
+import {
+    handleError,
+    ongewensteHTMLUitHeleDocument,
+    eersteLadingOverbodigeAttributesWeg,
+    maakMediaHTMLBronnen,
+    hinderlijkeTekstenEruitSlopen,
+    formatHTMLTextBodyNaarEenSpatieMax,
+    legeHTMLElementenVerwijderen,
+    maakTekstBlokHTML,
+    standaardSelectorConfig,
+    removeElementsRecursive
+} from "../gedeeld/longHTML.js";
+
 export default async function longTextSocialsIframes(page, event) {
-    return page.evaluate(
-        // eslint-disable-next-line no-shadow
-        ({ event }) => {
-            const res = {};
-            const textSelector = ".tribe-events-content";
-            const mediaSelector = [`${textSelector} iframe`].join(", ");
-            const removeEmptyHTMLFrom = textSelector;
+    const res = {
+        mediaForHTML: null,
+        textForHTML: null
+    };
+    // in standaard removeEls saveTheseAttrsFirst removeAttrsLastStep
+    // removeHTMLWithStrings htmlElementsWithStringsToRemove
+    const selectors = {
+        ...standaardSelectorConfig,
+        textBody: ".tribe-events-content",
+        mediaEls: [".tribe-events-content iframe"].join(", "),
+        removeEmptyHTMLFrom: ".tribe-events-content"
+    };
 
-            const removeSelectors = [
-                `${textSelector} [class*='icon-']`,
-                `${textSelector} [class*='fa-']`,
-                `${textSelector} .fa`,
-                `${textSelector} script`,
-                `${textSelector} noscript`,
-                `${textSelector} style`,
-                `${textSelector} meta`,
-                `${textSelector} svg`,
-                `${textSelector} form`,
-                `${textSelector} [aria-label='Videospeler']`,
-                `${textSelector} .video-shortcode`,
-                `${textSelector} img`
-            ].join(", ");
+    res.mediaForHTML = await maakMediaHTMLBronnen(page, selectors, event);
 
-            const attributesToRemove = [
-                "style",
-                "hidden",
-                "_target",
-                "frameborder",
-                "onclick",
-                "aria-hidden"
-            ];
-            const attributesToRemoveSecondRound = ["class", "id"];
-            const removeHTMLWithStrings = [];
+    await ongewensteHTMLUitHeleDocument(page, selectors);
 
-            const mediaAttrRemSelAdd = `${
-                mediaSelector.length
-                    ? `, ${mediaSelector} *, ${mediaSelector}`
-                    : ""
-            }`;
-            const textSocEnMedia = `${textSelector} ${mediaAttrRemSelAdd}`;
-            document.querySelectorAll(textSocEnMedia).forEach((elToStrip) => {
-                attributesToRemove.forEach((attr) => {
-                    if (elToStrip.hasAttribute(attr)) {
-                        elToStrip.removeAttribute(attr);
-                    }
-                });
-            });
+    await eersteLadingOverbodigeAttributesWeg(page, selectors);
 
-            //  media obj maken voordat HTML verdwijnt
-            res.mediaForHTML = Array.from(
-                document.querySelectorAll(mediaSelector)
-            ).map((bron) => {
-                const src = bron?.src ? bron.src : "";
-                return {
-                    outer: bron.outerHTML,
-                    src,
-                    id: null,
-                    type: src.includes("spotify")
-                        ? "spotify"
-                        : src.includes("youtube")
-                        ? "youtube"
-                        : "bandcamp"
-                };
-            });
+    await hinderlijkeTekstenEruitSlopen(page, selectors);
 
-            // stript HTML tbv text
-            removeSelectors.length &&
-                document
-                    .querySelectorAll(removeSelectors)
-                    .forEach((toRemove) =>
-                        toRemove.parentNode.removeChild(toRemove)
-                    );
+    await formatHTMLTextBodyNaarEenSpatieMax(page, selectors);
 
-            // verwijder ongewenste paragrafen over bv restaurants
-            removeHTMLWithStrings.length &&
-                Array.from(
-                    document.querySelectorAll(
-                        `${textSelector} p, ${textSelector} span, ${textSelector} a`
-                    )
-                ).forEach((verwijder) => {
-                    const heeftEvilString = !!removeHTMLWithStrings.find(
-                        (evilString) =>
-                            verwijder.textContent.includes(evilString)
-                    );
-                    if (heeftEvilString) {
-                        verwijder.parentNode.removeChild(verwijder);
-                    }
-                });
+    await removeElementsRecursive(page, selectors);
 
-            // lege HTML eruit cq HTML zonder tekst of getallen
-            // document.querySelectorAll(`${removeEmptyHTMLFrom} > *`)
-            //   .forEach(checkForEmpty => {
-            //     const leeg = checkForEmpty?.textContent.replace('&nbsp;','').replaceAll(/[\s\r\t]/g, '').trim() === '';
-            //     if (leeg){
-            //       checkForEmpty.parentNode.removeChild(checkForEmpty)
-            //     }
-            //   })
+    await legeHTMLElementenVerwijderen(page, selectors);
 
-            res.textForHTML = Array.from(
-                document.querySelectorAll(textSelector)
-            )
-                .map((el) => el.innerHTML)
-                .join("");
+    res.textForHTML = await maakTekstBlokHTML(page, selectors);
 
-            document
-                .querySelectorAll(textSelector)
-                .forEach((ts) => ts.setAttribute("data-text", "1"));
-
-            // laatste attributen eruit.
-            document.querySelectorAll(textSocEnMedia).forEach((elToStrip) => {
-                attributesToRemoveSecondRound.forEach((attr) => {
-                    if (elToStrip.hasAttribute(attr)) {
-                        elToStrip.removeAttribute(attr);
-                    }
-                });
-            });
-
-            // tekst
-            res.textForHTML = Array.from(
-                document.querySelectorAll("[data-text]")
-            )
-                .map((el) => el.innerHTML)
-                .join("");
-            return res;
-        },
-        { event }
-    );
+    return res;
 }
