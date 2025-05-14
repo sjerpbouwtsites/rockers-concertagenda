d88c83c900262dd08a52819a285b33821c9763ec
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2025-05-14 06:19:49 +0200

    event blocks image eigen component

--- src/mods/EventBlocks.jsx
+++ src/mods/EventBlocks.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BEMify, waitFor } from "./util.js";
 import closeIcon from "../images/close.png";
 import LoadmoreButton from "./LoadmoreButton.jsx";
+import EventBlocksUtil from "./EventBlocksUtil.jsx";
+import EventBlocksImage from "./EventBlocksImage.jsx";
 
 class EventBlocks extends React.Component {
     // #region constructor en life cycle
@@ -21,7 +23,6 @@ class EventBlocks extends React.Component {
 
         this.musicEventsLength = null;
         this.createLocationHTML = this.createLocationHTML.bind(this);
-        this.createDates = this.createDates.bind(this);
         this.add100ToMaxEventsShown = this.add100ToMaxEventsShown.bind(this);
         this.escFunction = this.escFunction.bind(this);
         this.hideSoldOut = this.hideSoldOut.bind(this);
@@ -65,142 +66,6 @@ class EventBlocks extends React.Component {
         }
     }
 
-    // eslint-disable-next-line
-    getSelectors(musicEvent, sharedModifiers, monthEvenOddCounter) {
-        const isEven = monthEvenOddCounter % 2 === 0;
-        return {
-            article: `
-provide-dark-contrast
-${isEven ? "provide-dark-contrast--variation-1" : ""}
-${BEMify("event-block", [
-    musicEvent.location.slug,
-    musicEvent.enlarged ? "enlarged" : "",
-    musicEvent.soldOut ? "sold-out" : "",
-    musicEvent.firstOfMonth ? "first-of-month" : "",
-    musicEvent.soldOut ? "sold-out" : "",
-    musicEvent.title.length > 36 ? "long-title" : "short-title",
-    musicEvent.title.length < 16 ? "tiny-title" : "",
-    musicEvent.longText ? "interactive" : "no-longTextHTML",
-    Math.random() > 0.8 ? "random-style" : "",
-    Math.random() > 0.5 ? "random-style-2" : "",
-    Math.random() > 0.5 ? "random-style-3" : ""
-])}`,
-
-            header: `${BEMify("event-block__header contrast-with-dark", sharedModifiers)}`,
-            headerH2: `${BEMify("contrast-with-dark event-block__title", sharedModifiers)}`,
-            headerEventTitle: `${BEMify(
-                "event-block__title-showname plain-sans-serif-font",
-                sharedModifiers
-            )}`,
-            headerLocation: `${BEMify(
-                "event-block__title-location color-green green-color event-block__header-text cursive-font",
-                sharedModifiers
-            )}`,
-            sluitEnlargedBtn: `${BEMify("event-block__sluit-enlarged-btn", sharedModifiers)}`,
-            image: BEMify("event-block__image", sharedModifiers),
-            dates: `${BEMify(
-                "event-block__dates event-block__header-text contrast-with-dark",
-                sharedModifiers
-            )}`,
-            headerShortText: `${BEMify(
-                "event-block__paragraph event-block__header-text contrast-with-dark",
-                ["short-text", ...sharedModifiers]
-            )} `,
-            main: `${BEMify("event-block__main contrast-with-dark", sharedModifiers)}`,
-            mainContainerForEnlarged: BEMify(
-                "void-container-for-enlarged",
-                sharedModifiers
-            ),
-            footer: `${BEMify("event-block__footer", sharedModifiers)} `,
-            hideSoldOutBtn: `${BEMify("event-block__hide-sold-out", sharedModifiers)} `
-        };
-    }
-
-    createDates(musicEvent) {
-        // FIXME naar eigen component
-        const start = new Date(musicEvent.start);
-        const enlargedBEM = musicEvent.enlarged
-            ? "event-block__dates--enlarged"
-            : "";
-
-        let year;
-        if (
-            musicEvent.enlarged &&
-            musicEvent.start.substring(0, 4) === this.currentYear
-        ) {
-            year = "numeric";
-        }
-        const startDateText = start.toLocaleDateString("nl", {
-            // weekday: musicEvent.enlarged ? "short" : undefined,
-            month: musicEvent.enlarged ? "2-digit" : "2-digit",
-            day: "numeric",
-            year,
-            hour: "2-digit",
-            minute: "2-digit"
-        });
-        const startDateHTML = `<time className="event-block__dates event-block__dates--start-date ${enlargedBEM}" dateTime="${musicEvent.start}">${startDateText}</time>`;
-        if (!musicEvent.enlarged) {
-            return startDateHTML;
-        }
-
-        let openDoorDateHTML = "";
-        if (musicEvent.door) {
-            const deurTijd = musicEvent.door.match(/T(\d\d:\d\d)/)[1];
-            openDoorDateHTML = `<time className="event-block__dates event-block__dates--door-date ${enlargedBEM}" dateTime="${musicEvent.door}">deur: ${deurTijd}</time>`;
-        }
-
-        let endDateHTML = "";
-        if (musicEvent.end) {
-            // const endText = (new Date(musicEvent.door)).toLocaleDateString("nl", {
-            //   hour: "2-digit",
-            //   minute: "2-digit",
-            // });
-            const eindTijd = musicEvent.end.match(/T(\d\d:\d\d)/)[1];
-            endDateHTML = `<time className="event-block__dates event-block__dates--end-date ${enlargedBEM}" dateTime="${musicEvent.end}">eind: ${eindTijd}</time>`;
-        }
-
-        return `${startDateHTML}${openDoorDateHTML}${endDateHTML}`;
-    }
-
-    // eslint-disable-next-line
-    createImageHTML(musicEvent, selectors) {
-        if (musicEvent?.image?.includes("https") ?? false) {
-            return (
-                <img
-                    src={musicEvent.image}
-                    width="440"
-                    height="220"
-                    className={selectors.image}
-                    alt={musicEvent.title}
-                />
-            );
-        }
-
-        let imgSrc = musicEvent.image // FIXME naar eigen component
-            ? `${musicEvent.image}`
-            : `/location-images/${musicEvent.location.slug}`;
-
-        imgSrc = imgSrc.replace("../public", "");
-
-        const srcset = `${imgSrc}-w750.webp 750w`;
-        const sizes = "(min-width: 480px) 750px";
-        const src = `${imgSrc}-w440.webp`;
-
-        return imgSrc ? (
-            <img
-                src={src}
-                srcSet={srcset}
-                sizes={sizes}
-                width="440"
-                height="220"
-                className={selectors.image}
-                alt={musicEvent.title}
-            />
-        ) : (
-            ""
-        );
-    }
-
     createLocationHTML(musicEvent) {
         return (
             <span>
@@ -223,20 +88,6 @@ ${BEMify("event-block", [
                 {musicEvent.shortText}
             </p>
         );
-    }
-
-    // eslint-disable-next-line
-    createShortestText(musicEvent) {
-        // eslint-disable-line
-        if (!musicEvent.shortText) return "";
-        const m = 15;
-        const splitted = musicEvent.shortText?.split(" ") ?? null;
-        if (!splitted) return "";
-        let s = splitted.splice(0, m).join(" ") ?? "";
-        if (splitted.length > m) {
-            s += "...";
-        }
-        return s;
     }
 
     async recursieveStijlEraf() {
@@ -530,51 +381,6 @@ ${BEMify("event-block", [
         // })
     }
 
-    // eslint-disable-next-line
-    bewerktMusicEventTitle(musicEvent) {
-        const met = musicEvent.title;
-        let shortestText = this.createShortestText(musicEvent);
-        const res = {};
-        const titleIsCapsArr = met
-            .split("")
-            .map((char) => char === char.toUpperCase());
-        const noOfCapsInTitle = titleIsCapsArr.filter((a) => a).length;
-        const metl = met.length;
-        const toManyCapsInTitle = (metl - noOfCapsInTitle) / metl < 0.5;
-        if (toManyCapsInTitle) {
-            // eslint-disable-next-line
-            res.title =
-                met[0].toUpperCase() + met.substring(1, 500).toLowerCase();
-        } else {
-            res.title = met;
-        }
-
-        if (res.title.length > 45) {
-            const splittingCandidates = ["+", "&", ":", ">", "•"];
-            let i = 0;
-            do {
-                const reg2 = RegExp(`/.*${i}/`);
-                const reg1 = RegExp(`/${i}.*/`);
-                const beginDeel = res.title.replace(reg1, "").trim();
-                const tweedeDeel = res.title.replace(reg2, "").trim();
-                res.title = beginDeel;
-                shortestText = `${tweedeDeel} ${shortestText}`;
-                i += 1;
-            } while (res.title.length > 45 && i < splittingCandidates.length);
-            shortestText =
-                shortestText[0].toUpperCase() +
-                shortestText.substring(1, 500).toLowerCase();
-        }
-
-        if (res.title.length > 45) {
-            res.title = res.title
-                .replace(/\(.*\)/, "")
-                .replace(/\s{2,25}/, " ");
-        }
-        res.shortestText = shortestText;
-        return res;
-    }
-
     /**
      * Pas vlak voor de render weet je, want na filtering,
      * welke musicEvents de eerste van de maand zijn.
@@ -621,7 +427,11 @@ ${BEMify("event-block", [
         const mel = filteredMusicEvents.length;
         let monthEvenOddCounter = 0;
 
-        const printThis = this.addFirstOfMonth(filteredMusicEvents);
+        const printThis = EventBlocksUtil.addFirstOfMonth(
+            filteredMusicEvents
+                // bewerktMusicEventTitle doet allemaal vage dingen door elkaar sooals shortesttesttext maken
+                .map(EventBlocksUtil.bewerktMusicEventTitle)
+        );
 
         return (
             <div
@@ -647,23 +457,21 @@ ${BEMify("event-block", [
                                 musicEvent.soldOut ? "sold-out" : "",
                                 musicEvent.enlarged ? "enlarged" : ""
                             ];
-                            const selectors = this.getSelectors(
+                            const selectors = EventBlocksUtil.getSelectors(
                                 musicEvent,
                                 sharedModifiers,
                                 monthEvenOddCounter
                             );
                             const priceElement = this.priceElement(musicEvent);
-                            const datesHTML = this.createDates(musicEvent);
+                            const datesHTML =
+                                EventBlocksUtil.createDates(musicEvent);
                             const hideSoldOutBtn = this.createHideSoldOutBtn(
                                 musicEvent,
                                 selectors
                             );
                             const numberOfDates =
                                 datesHTML.match(/<time/g).length;
-                            const imageHTML = this.createImageHTML(
-                                musicEvent,
-                                selectors
-                            );
+
                             const articleID = `event-id-${musicEventKey}`;
                             const firstOfMonthBlock =
                                 musicEvent.firstOfMonth ? (
@@ -678,9 +486,6 @@ ${BEMify("event-block", [
                                 musicEvent,
                                 selectors
                             );
-
-                            const titelEnShortText =
-                                this.bewerktMusicEventTitle(musicEvent);
 
                             return (
                                 // eslint-disable-next-line
@@ -706,7 +511,10 @@ ${BEMify("event-block", [
                                     }
                                 >
                                     {firstOfMonthBlock}
-                                    {imageHTML}
+                                    <EventBlocksImage
+                                        musicEvent={musicEvent}
+                                        imageClassName={selectors.image}
+                                    />
                                     <header className={selectors.header}>
                                         <h2 className={selectors.headerH2}>
                                             <span
@@ -714,10 +522,10 @@ ${BEMify("event-block", [
                                                     selectors.headerEventTitle
                                                 }
                                                 data-short-text={
-                                                    titelEnShortText.shortestText
+                                                    musicEvent.shortestText
                                                 }
                                             >
-                                                {titelEnShortText.title}
+                                                {musicEvent.title}
                                             </span>
                                             <span
                                                 className={`${selectors.headerLocation} number-of-dates-${numberOfDates}`}
